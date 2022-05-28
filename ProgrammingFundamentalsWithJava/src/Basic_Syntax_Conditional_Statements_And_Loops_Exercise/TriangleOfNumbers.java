d4062a4ff93d802caa2d2ced3a35c00365b9e622
Pursi <pursib@abv.bg>
2022-05-28 22:43:55 +0300

    Triangle of Numbers task Done.

---
+++ ProgrammingFundamentalsWithJava/src/Basic_Syntax_Conditional_Statements_And_Loops_Exercise/TriangleOfNumbers.java
@@ -0,0 +1,18 @@
+package Basic_Syntax_Conditional_Statements_And_Loops_Exercise;
+
+import java.util.Scanner;
+
+public class TriangleOfNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
